1a9b375eda575eeeb34970c27e1b30e9cc575ea1
Łukasz Duda <lukasz.duda.pl@gmail.com>
2022-10-23 17:55:21 +0000

    Load CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
 };
