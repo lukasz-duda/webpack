81cdf1b41d6ec6351a0f0d031855a50f90608bec
Łukasz Duda <lukasz.duda.pl@gmail.com>
2022-10-24 19:36:34 +0000

    Use image

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg)$/i,
+                type: 'asset/resource'
             }
         ]
     }
