2b1b8662f5e88b16db4fa0ebc308ae0d5314894f
Łukasz Duda <lukasz.duda.pl@gmail.com>
2022-10-24 19:49:36 +0000

    Use plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -6,10 +7,15 @@ module.exports = {
         index: './src/index.js',
         print: './src/print.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output managment'
+        })
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: false
+        clean: true
     },
     module: {
         rules: [
